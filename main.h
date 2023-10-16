b37ddddc68c36d5d56fcef53f8d1129a5a9ceb47
YaraAminn <aminyara.94@gmail.com>
2023-10-16 12:15:20 -0700

    creating strLen files

--- main.h
+++ main.h
@@ -6,5 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 int _printf(const char *format, ...);
-
+int _putchar(char c);
+int strLen(char *size);
+int lenStr_Ptr(const char *size);
 #endif
---
+++ strLenth.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * strLen - function to get length of string
+ * @size: char pointer
+ *
+ * Return: i success
+ */
+int strLen(char *size)
+{
+	int i;
+
+	for (i = 0; size[i] != 0; i++)
+	{
+		return(i);
+	}
+}
+
+/**
+ * lenStr_Ptr - function to get the size of
+ * the size pointer which is constant
+ *
+ * @size: char pointer const
+ * Return: j success
+ */
+int lenStr_Ptr(const char *size)
+{
+	int j;
+
+	for (j = 0; size[j] != 0; j++)
+	{
+		return (j);
+	}
+}
