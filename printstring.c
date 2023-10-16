2e692313d687d70d03772cb5e960fab5d61a79c0
YaraAminn <aminyara.94@gmail.com>
2023-10-16 14:22:19 -0700

    edit string file

--- printstring.c
+++ printstring.c
@@ -14,13 +14,21 @@ int string_print(va_list str)
 	s = va_arg(str, char *);
 	if (s == NULL)
 	{
-		return (-1);
+		s = "(Nothing)";
+		length = strlen(s);
+		for (i = 0; i < length; i++)
+		{
+			_putchar([i]);
+		}
+		return (length);
 	}
 	else
 	{
 		length = strLen(s);
 		for (i = 0; i < length; i++)
+		{
 			_putchar(s[i]);
+		}
 		return (length);
 	}
 }
